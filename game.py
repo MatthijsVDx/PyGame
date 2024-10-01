3b23515b9d08ce3b74573ad653238e66930938bb
justcoding-a <anthilori25@gmail.com>
2024-10-01 15:41:56 +0200

    Heb paar aanpassingen gedaan

--- game.py
+++ game.py
@@ -1,38 +1,40 @@
 import pygame
 pygame.init()
 
-screen_width = 800
+
 screen_height = 600
-screen = pygame.display.set_mode((screen_height, screen_width))
+screen_width = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 player = pygame.Rect((300,250,50,50))
 
 run = True
 while run:
     
-    
+    player.move_ip(1,1)
+    screen.fill((0,0,0))
     pygame.draw.rect(screen,(255,0,0), player)
     
     key = pygame.key.get_pressed()
-#Moving left
-    if key [pygame.K_a] == True:
-        player.move_ip(-1, 0)
-#Moving right
-    elif key [pygame.K_d] == True:
-        player.move_ip(1, 0)
+
 #Moving up
-    elif key [pygame.K_w] == True:
-        player.move_ip(0, -1)
-#Moving down
-    elif key [pygame.K_s] == True:
-        player.move_ip(0, 1)
+    if key [pygame.K_w] == True:
+        player.move_ip(0, -2)
+
+    elif key [pygame.K_ESCAPE] == True:
+        pygmae.QUIT
     
+    white = (255,255,255)
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    groundstart_pos = (200, 200)
+    groundend_pos = (800, 200)
+    ground_height = 10
+    
+    pygame.draw.line(screen, white, groundstart_pos, groundend_pos, ground_height)
+    
+ 
             
-    pygame.display.update()
-            
+    pygame.display.update()        
 pygame.quit()
 
