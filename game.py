d958da00483974221c6f11f4f14a6666bf7dc824
justcoding-a <anthilori25@gmail.com>
2024-10-04 10:13:05 +0200

    beetje beweging

--- game.py
+++ game.py
@@ -15,11 +15,21 @@ test_font = pygame.font.Font( "font\Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics\Sky.png").convert()
 ground_surface = pygame.image.load("graphics\ground.png").convert()
-text_surface = test_font.render("My Game", False, "black").convert()
 
-enemy_surface = pygame.image.load("graphics\snail\snail1.png").convert_alpha()
-snail_x_pos = 900
-snail_y_pos = 275
+
+score_surf = test_font.render("My Game", False, "black").convert()
+score_rect = score_surf.get_rect(center = (400, 50))
+
+
+
+snail_surface = pygame.image.load("graphics\snail\snail1.png").convert_alpha()
+snail_hitbox  = snail_surface.get_rect(midbottom = (400, 312.5))
+
+
+
+
+player_surface = pygame.image.load("graphics\Player\player_walk_1.png").convert_alpha()
+player_hitbox = player_surface.get_rect( midbottom = (80, 312.5))
 
 while True:
     
@@ -28,23 +38,35 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+            
+    
+#    if event.type == pygame.MOUSEMOTION:
+#        if player_hitbox.collidepoint(event.pos):
+#            print("Collison")
+        
+    
     
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300,50))
+    
+    pygame.draw.rect(screen, "pink", score_rect)
+    pygame.draw.rect(screen, "pink", score_rect, 10)
+    
+    
+    screen.blit(score_surf, score_rect)
+    screen.blit(player_surface, player_hitbox)
+    screen.blit(snail_surface, snail_hitbox)
+    
+    snail_hitbox.left += -4
+    
 
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos += 900
-    screen.blit(enemy_surface, (snail_x_pos, snail_y_pos))
+    if snail_hitbox.right < 0:
+        snail_hitbox.left += 900
+
 
     
     
     
-    
-    
-    
-    
-    #framerate
+#framerate
     clock.tick(60)
     pygame.display.update()
